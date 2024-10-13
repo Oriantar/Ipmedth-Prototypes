f6ed88bec4bf5d9ada8f1033b9705090e4e1573c
Tim Maurizio <tim.maurizio@gmail.com>
2024-10-13 17:35:21 +0200

    removed unused js files

--- ThreeJs/js/geometry.js
+++
@@ -1,3 +0,0 @@
-export const cube = new THREE.BoxGeometry(1, 1, 1);
-export const sphere = new THREE.SphereGeometry(1, 32, 32);
-export const plane = new THREE.PlaneGeometry(5, 5);
--- ThreeJs/js/materials.js
+++
@@ -1,3 +0,0 @@
-export const red = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-export const green = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-export const blue = new THREE.MeshBasicMaterial({ color: 0x0000ff });
